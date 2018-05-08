1ac8f71723f06700f0afc199952ca7673787dfe1
Ryan Holmdahl <ryanlh@stanford.edu>
2018-05-07 17:37:30 -0700

    add repl->engine communication functions

--- xnode/lib/repl.js
+++ xnode/lib/repl.js
@@ -3,7 +3,7 @@
 import PythonShell from 'python-shell';
 import { spawn } from 'child_process';
 
-const EXECUTE_PATH = './resources/execute.py';
+const EXECUTE_PATH = './engine.py';
 
 export default class REPL {
 
@@ -13,16 +13,10 @@ export default class REPL {
      * @param {string} scriptPath
      *     The path to the script that the engine should execute. The engine remains associated with this script,
      *     rerunning if needed after every file edit.
-     * @param {Object} watchStatements
-     *     A collection of statements which indicate which variables in the Python program should be evaluated and
-     *     at which line evaluation should take place.
-     * @param {Function} onUpdate
-     *     A function of signature `(symbolData)` to be called whenever a variable is evaluated in the Python program.
      */
     constructor(scriptPath) {
-        this.scriptPath = scriptPath;
         this.watchStatements = [];
-        this.executionEngine = null;
+        this.executionEngine = startEngine(scriptPath);
 
         // TODO intialize store
 
@@ -33,47 +27,70 @@ export default class REPL {
     }
 
     /**
-     * Called by the package whenever a file is updated.
+     * Creates a new execution engine.
      *
-     * The engine should determine whether or not the change requires a re-run of the Python script, and which parts
-     * of the script must be run.
+     * The engine is a Python process, which persists for the lifespan of the sandbox. Changes to files and watch
+     * statements are relayed to the engine, which potentially runs some or all of `scriptPath` and relays any watched
+     * data to REPL, which stores that data.
      *
-     * @param  {string} filePath
-     *     Path to the file that was changed.
-     * @param  {Object} diff
-     *     Indicates what part of the file changed.
+     * @param  {string} scriptPath
+     *      The path to the Python script whose data should be visualized in the canvas.
+     * @returns {PythonShell}
+     *      A Python subprocess with which REPL can communicate to acquire evaluated watch statements.
      */
-    onFileChanged(filePath, diff) {
-        // TODO check file to see if relevant, caching
-        this.endExistingShell();
-        this.run();
-    }
-
-    endExistingShell() {
-        // TODO test this; could be that shell might have already ended? also, does this kill the shell immediately?
-        //
-        if (this.shell !== null) {
-            this.shell.end();
-            this.shell = null;
-        }
-    }
-
-    /**
-     * Runs the entirety of the associated Python script.
-     *
-     * TODO This function should be changed or removed as caching is added.
-     */
-    run() {
+    startEngine(scriptPath) {
         let options = {
-            args: ['c:\\users\\ryan holmdahl\\documents\\github\\xnode\\xnode\\xnode\\lib\\dummy.py:10', '--script', './lib/dummy.py'],
+            args: [scriptPath],
         }
-        this.shell = new PythonShell(EXECUTE_PATH, options);
-        let onUpdate = this.onUpdate;
-        this.shell.on('message', (message) => {
+        let executionEngine = new PythonShell(EXECUTE_PATH, options);
+        executionEngine.on('message', (message) => {
             console.log(message);
             let { data, shells } = JSON.parse(message);
             // TODO store data and shells locally
-            onUpdate(data);
         });
+        return executionEngine;
+    }
+
+    /**
+     * Toggles the existence of a watch statement at a given line.
+     *
+     * @param  {string} filePath
+     *      Path to the file whose line should be watched.
+     * @param  {number} lineNum
+     *      The line number in `filePath` to be watched.
+     * @param  {?string} action
+     *      Currently unused; TODO: the expression to be performed on the watched variable.
+     */
+    toggleWatchStatement(filePath, lineNum, action = null) {
+        this.executionEngine.send(`watch:${filePath}?${lineNum}?${action}`)
+    }
+
+    /**
+     * Fetches the data object for a given symbol from the execution engine.
+     *
+     * The data object is not directly returned, but will eventually be sent by the execution engine to REPL as a
+     * message, at which point it is added to the symbol table.
+     *
+     * @param  {string} symbolId
+     *      The identifier of the symbol, as acquired from a reference in the symbol table.
+     */
+    fetchSymbolData(symbolId) {
+        this.executionEngine.send(`fetch:${symbolId}`);
+    }
+
+    /**
+     * Called by the package whenever a file is updated.
+     *
+     * Sends the diff to the engine, which decides what parts, if any, of the script need to be rerun.
+     *
+     * @param  {string} filePath
+     *     Path to the file that was changed.
+     * @param  {object} diff
+     *     Indicates what part of the file changed.
+     */
+    onFileChanged(filePath, diff) {
+        // TODO: convert diff to string that is understood by engine
+        diff = '';
+        this.executionEngine.send(`change:${filePath}?${diff}`)
     }
 }
