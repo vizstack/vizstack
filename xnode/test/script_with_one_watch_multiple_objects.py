2681c2f0996650382decbdaaaa30790bc556df1f
Ryan Holmdahl <ryanlh@stanford.edu>
2018-10-19 17:32:26 -0700

    remove extra lines from test scripts

--- xnode/test/script_with_one_watch_multiple_objects.py
+++ xnode/test/script_with_one_watch_multiple_objects.py
@@ -3,5 +3,3 @@ import xn
 myInt = 69
 myInt2 = 96
 xn.view(myInt, myInt2)
-# Currently, xn.view() doesn't work if it's the last line of the file
-myInt3 = 70
--- xnode/test/script_with_one_watch_no_objects.py
+++ xnode/test/script_with_one_watch_no_objects.py
@@ -1,5 +1,3 @@
 import xn
 
 xn.view()
-# Currently, xn.view() doesn't work if it's the last line of the file
-myInt = 70
