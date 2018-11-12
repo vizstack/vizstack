c22cb8b44c4f237654c7d1076b0193b5730e4f4d
Nikhil Bhattasali <nikhilxb@stanford.edu>
2018-11-12 13:27:52 -0800

    Remove ugly code

--- xnode/src/services/path-utils.js
+++ xnode/src/services/path-utils.js
@@ -3,50 +3,6 @@
  */
 
 /**
- * Returns a mapping of path names to their minimal unique name among the set of all paths.
- * @param paths
- *      A list of path name strings.
- * @returns
- *      An object mapping path name strings to their unique shortened versions.
- */
-// TODO: Remove this.
-export function getMinimalUniquePaths(paths: Array<string>): {[string]: string} {
-    let pathToTokens = {};
-    paths.forEach(path => pathToTokens[path] = path.split(/[/\\]+/));
-    let pathToCurrentTokenIndex = {};
-    paths.forEach(path => pathToCurrentTokenIndex[path] = pathToTokens[path].length - 2);
-    let pathToShortened = {};
-    let shortenedToPaths = {};
-    Object.entries(pathToTokens).forEach(([path, tokens]) => {
-        const token = tokens[tokens.length - 1];
-        pathToShortened[path] = token;
-        if (!(token in shortenedToPaths)) {
-            shortenedToPaths[token] = new Set();
-        }
-        shortenedToPaths[token].add(path);
-    });
-    paths.forEach(path => {
-        let shortened = pathToShortened[path];
-        while(shortenedToPaths[shortened].size > 1) {
-            shortenedToPaths[shortened].forEach(conflictingPath => {
-                const currentTokenIndex = pathToCurrentTokenIndex[conflictingPath];
-                if (currentTokenIndex >= 0) {
-                    pathToShortened[conflictingPath] = pathToTokens[conflictingPath][currentTokenIndex] + "/" + shortened;
-                    pathToCurrentTokenIndex[conflictingPath] -= 1;
-                    if (!(pathToShortened[conflictingPath] in shortenedToPaths)) {
-                        shortenedToPaths[pathToShortened[conflictingPath]] = new Set();
-                    }
-                    shortenedToPaths[pathToShortened[conflictingPath]].add(conflictingPath);
-                }
-            });
-            shortenedToPaths[shortened] = new Set([...shortenedToPaths[shortened]].filter(conflictPath => pathToShortened[conflictPath] === shortened));
-        }
-    });
-    return pathToShortened;
-}
-
-
-/**
  * Returns a map from unique filepaths to their minimal disambiguated path suffixes (e.g. filepaths in an IDE tab to
  * distinguish between different open files).
  * @param paths
