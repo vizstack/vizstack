667079c6f01480925e04d6647dfb10f72d9cebe1
Nikhil Bhattasali <nikhilxb@stanford.edu>
2019-02-26 17:31:04 -0800

    Reordered demo

--- python/demo/demo.py
+++ python/demo/demo.py
@@ -1,26 +1,19 @@
 import xnode as xn
-from demo_classes import Company
+from demo_classes import Company, import_employees, import_products
 
 if __name__ == '__main__':
-    company = Company("Xnode, Inc.")
-    loc = company.add_location("Stanford")
+    employees = import_employees("employees.tsv")
+    products = import_products("products.tsv")
 
-    employees = loc.import_employees("employees.tsv")
     # print(employees)
-    xn.show(employees)
-    # xn.show([e for idx, e in enumerate(employees.values()) if idx <= 50])
-    #
-    # products = loc.import_products("products.tsv")
+    # print(products)
+
+    # xn.show(employees)
     # xn.show(products)
 
-    # assert False
+    # company = Company("Xnode, Inc.")
+    # loc = company.add_location("Stanford", employees, products)
     # loc.update_inventory(1525, 30)
     # loc.update_inventory(5220, 50)
-
-    # xn.show(company)
-    # xn.show(Company)
     #
-    # def hi(a, b, c="4", d="herro", e=None):
-    #     pass
-    # xn.show(hi)
-    # xn.show(lambda x=None: x + 1)
+    # xn.show(company)
