12a1911283291a141100ee655fa4c8de0c1017c5
Ryan Holmdahl <ryholmdahl@gmail.com>
2019-09-07 17:58:23 -0700

    Update Python bindings description

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='vizstack-py',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     packages=['vizstack'],
     zip_safe=False,
     author='Ryan Holmdahl & Nikhil Bhattasali',
+    author_email='vizstack@gmail.com',
     install_requires=['cuid', 'mypy_extensions', 'typing_extensions'],
     description="Generate Vizstack visualizations of Python programs.",
     url='https://github.com/vizstack/vizstack',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
